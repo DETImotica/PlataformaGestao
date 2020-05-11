a01d942389719b891046fc3bc1371e2fdd347da6
Rodrigo Rosmaninho <r.rosmaninho@ua.pt>
2020-05-11 18:11:47 +0100

    Harden security, prepare for additional api requests, add 'reveal AES key' Modal, minor ui fixes

--- sensors/urls.py
+++ sensors/urls.py
@@ -4,13 +4,16 @@ from . import views
 
 app_name = 'sensors'
 urlpatterns = [
+    path('', views.template, name='template'),
+    path('rooms/', views.RoomsIndexView.as_view(), name='rooms'),
+    path('sensors/', views.SensorsIndexView.as_view(), name='sensors'),
+    path('types/', views.TypesIndexView.as_view(), name='types'),
+    path('sensors/<slug:room_id>/', views.SensorsIndexView.roomDetails, name="roomDetails"),
+
     path('login/', views.login, name='login'),
     path('api_login/', views.api_login, name='api_login'),
     path('logout/', views.logout, name='logout'),
     path('forbidden/', views.forbidden, name='forbidden'),
-    path('rooms/', views.RoomsIndexView.as_view(), name='rooms'),
-    path('sensors/', views.SensorsIndexView.as_view(), name='sensors'),
-    path('types/', views.TypesIndexView.as_view(), name='types'),
-    path('', views.template, name='template'),
-    path('sensors/<slug:room_id>/', views.SensorsIndexView.roomDetails, name="roomDetails"),
+
+    path('get/key/<slug:uuid>', views.key, name='api_key')
 ]
--- sensors/views.py
+++ sensors/views.py
@@ -1,12 +1,14 @@
 # from django.shortcuts import render
 
 # Create your views here.
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect
 from django.views import generic
-import requests
+import requests, json
 
 from .models import Room, Sensor, Type
+
+# Main views
 
 class RoomsIndexView(generic.ListView):
     template_name = 'sensors/rooms.html'
@@ -15,7 +17,14 @@ class RoomsIndexView(generic.ListView):
     def get(self, *args, **kwargs):
         if 'cookies' not in self.request.session:
             return redirect('/login')
-        return super(RoomsIndexView, self).get(*args, **kwargs)
+
+        try:
+            return super(RoomsIndexView, self).get(*args, **kwargs)
+        except ResponseException as r:
+            if r.code == 401:
+                return redirect('/logout')
+            else:
+                raise Exception
 
     def get_context_data(self, **kwargs):
         ctx = super(RoomsIndexView, self).get_context_data(**kwargs)
@@ -32,14 +41,13 @@ class RoomsIndexView(generic.ListView):
         raise Http404("Não existe nenhuma sala")
 
 def loadRooms(self):
-    for i in range(3):
-        requestRoomsID = api_get_request('/rooms', self.request.session)
-        if requestRoomsID.headers["Content-Type"]=="application/json":
-            roomsList = []
-            for room in requestRoomsID.json()["ids"]:
-                request = api_get_request('/room/' + room, self.request.session).json()
-                roomsList.append(Room(room_id=room,name=request['name'],description=request['description']))
-            return roomsList
+    requestRoomsID = api_get_request('/rooms', self.request.session)
+    if requestRoomsID.headers["Content-Type"]=="application/json":
+        roomsList = []
+        for room in requestRoomsID.json()["ids"]:
+            request = api_get_request('/room/' + room, self.request.session).json()
+            roomsList.append(Room(room_id=room,name=request['name'],description=request['description']))
+        return roomsList
     return []
 
 class SensorsIndexView(generic.ListView):
@@ -53,39 +61,43 @@ class SensorsIndexView(generic.ListView):
         raise Http404("Não existe nenhuma sala")
 
     def loadRoom(session, id):
-        for i in range(3):
-            requestRoomsID = api_get_request('/rooms', session)
-            if requestRoomsID.headers["Content-Type"]=="application/json":
-                for room in requestRoomsID.json()["ids"]:
-                    if room==id:
-                        request = api_get_request('/room/' + room, session).json()
-                        return Room(room_id=room,name=request['name'],description=request['description'])
-                return []
+        requestRoomsID = api_get_request('/rooms', session)
+        if requestRoomsID.headers["Content-Type"]=="application/json":
+            for room in requestRoomsID.json()["ids"]:
+                if room==id:
+                    request = api_get_request('/room/' + room, session).json()
+                    return Room(room_id=room,name=request['name'],description=request['description'])
+            return []
         return []
 
     def loadRoomSensors(session, room_id):
-        for i in range(3):
-            requestSensorsID = api_get_request('/room/' + room_id + '/sensors', session)
-            if requestSensorsID.headers["Content-Type"]=="application/json":
-                sensorsList = []
-                for sensor in requestSensorsID.json()["ids"]:
-                    request = api_get_request('/sensor/' + sensor, session).json()
-                    sensorsList.append(Sensor(sensor_id=sensor,room_id=request['room_id'],description=request['description'],type=request['data']['type'],symbol=request['data']['unit_symbol']))
-                return sensorsList
+        requestSensorsID = api_get_request('/room/' + room_id + '/sensors', session)
+        if requestSensorsID.headers["Content-Type"]=="application/json":
+            sensorsList = []
+            for sensor in requestSensorsID.json()["ids"]:
+                request = api_get_request('/sensor/' + sensor, session).json()
+                sensorsList.append(Sensor(sensor_id=sensor,room_id=request['room_id'],description=request['description'],type=request['data']['type'],symbol=request['data']['unit_symbol']))
+            return sensorsList
         return []
 
     def roomDetails(request, room_id):
         if 'cookies' not in request.session:
             return redirect('/login')
 
-        room = SensorsIndexView.loadRoom(request.session, room_id)
-        if room != []:
-            sensors = SensorsIndexView.loadRoomSensors(request.session, room_id)
-            if sensors != []:
-                types = loadTypes(request.session)
-                return render(request, 'sensors/roomDetails.html', {'room': room, 'sensors': sensors, 'types': types, 'uname': request.session['uname']})
-            return render(request, 'sensors/roomDetails.html', {'room': room, 'uname': request.session['uname']})
-        raise Http404("Sala não existente")
+        try:
+            room = SensorsIndexView.loadRoom(request.session, room_id)
+            if room != []:
+                sensors = SensorsIndexView.loadRoomSensors(request.session, room_id)
+                if sensors != []:
+                    types = loadTypes(request.session)
+                    return render(request, 'sensors/roomDetails.html', {'room': room, 'sensors': sensors, 'types': types, 'uname': request.session['uname']})
+                return render(request, 'sensors/roomDetails.html', {'room': room, 'uname': request.session['uname']})
+            raise Http404("Sala não existente")
+        except ResponseException as r:
+            if r.code == 401:
+                return redirect('/logout')
+            else:
+                raise Exception
 
 class TypesIndexView(generic.ListView):
     template_name = 'sensors/types.html'
@@ -94,7 +106,14 @@ class TypesIndexView(generic.ListView):
     def get(self, *args, **kwargs):
         if 'cookies' not in self.request.session:
             return redirect('/login')
-        return super(TypesIndexView, self).get(*args, **kwargs)
+
+        try:
+            return super(TypesIndexView, self).get(*args, **kwargs)
+        except ResponseException as r:
+            if r.code == 401:
+                return redirect('/logout')
+            else:
+                raise Exception
 
     def get_context_data(self, **kwargs):
         ctx = super(TypesIndexView, self).get_context_data(**kwargs)
@@ -108,14 +127,13 @@ class TypesIndexView(generic.ListView):
         raise Http404("Não existe nenhuma métrica")
 
 def loadTypes(session):
-    for i in range(3):
-        requestTypes = api_get_request('/types', session)
-        if requestTypes.headers["Content-Type"]=="application/json":
-            typesList = []
-            for type in requestTypes.json()["types"]:
-                #request = api_get_request('/type/' + type, session).json()
-                typesList.append(Type(metric=type))
-            return typesList
+    requestTypes = api_get_request('/types', session)
+    if requestTypes.headers["Content-Type"]=="application/json":
+        typesList = []
+        for type in requestTypes.json()["types"]:
+            #request = api_get_request('/type/' + type, session).json()
+            typesList.append(Type(metric=type))
+        return typesList
     return []
 
 def template(request):
@@ -128,6 +146,17 @@ def template(request):
         return redirect('/login')
     return render(request, "sensors/index.html", {'uname': request.session['uname']})
 
+# API Requests
+
+def key(request, uuid):
+    try:
+        return HttpResponse('{"key": "placeholder"}', content_type='application/json')
+        #return HttpResponse(api_get_request('/key/' + uuid, request.session), content_type='application/json')
+    except ResponseException as r:
+        return HttpResponse(status=r.code)
+
+
+# Session views
 
 def login(request):
     return render(request, "sensors/login.html")
@@ -136,8 +165,11 @@ def login(request):
 def logout(request):
     if 'cookies' not in request.session:
         return redirect('/login')
-    for i in range(3):
+    try:
         api_get_request('/logout', request.session)
+    except ResponseException as r:
+        if r.code != 401:
+            raise Exception
     request.session.flush()
     return redirect('/login')
 
@@ -149,10 +181,38 @@ def forbidden(request):
 def api_login(request):
     return redirect('https://detimotic-aulas.ws.atnog.av.it.pt/api/v1/login?app=gestao&redirect_url=' + request.build_absolute_uri('/'))
 
+# API Requests - Helper functions
 
-def api_get_request(endpoint, session):
-    return requests.get('https://detimotic-aulas.ws.atnog.av.it.pt/api/v1' + endpoint, headers={'User-Agent': session['User-Agent']}, verify=False, cookies=session['cookies'])
+def api_get_request(endpoint, session, tries=0):
+    result = requests.get('https://detimotic-aulas.ws.atnog.av.it.pt/api/v1' + endpoint, headers={'User-Agent': session['User-Agent']}, verify=False, cookies=session['cookies'])
+    if result.status_code == 200:
+        if tries < 3:
+            return result if is_json(result.text) else api_get_request(endpoint, session, tries + 1)
+        else:
+            return None
+    else:
+        raise ResponseException(result.status_code)
 
+
+def api_post_request(endpoint, data, session, tries=0):
+    result = requests.post('https://detimotic-aulas.ws.atnog.av.it.pt/api/v1' + endpoint, data, headers={'User-Agent': session['User-Agent']}, verify=False, cookies=session['cookies'])
+    if result.status_code == 200:
+        if tries < 3:
+            return result if is_json(result.text) else api_post_request(endpoint, data, session, tries + 1)
+        else:
+            return None
+    else:
+        raise ResponseException(result.status_code)
+
+
+def is_json(data):
+    try:
+        json_object = json.loads(data)
+    except ValueError as e:
+        return False
+    return True
+
+# Handlers
 
 def handler404(request, exception):
     return render(request, 'sensors/404.html', status=404)
@@ -160,3 +220,9 @@ def handler404(request, exception):
 
 def handler500(request):
     return render(request, 'sensors/500.html', status=500)
+
+# Custom Exception
+
+class ResponseException(Exception):
+    def __init__(self, *args):
+        self.code = args[0] if args else 500
